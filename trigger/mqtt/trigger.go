495bd0714156bc598afa34160652f49b6498825d
Andy Hampshire <ahampshi@tibco.com>
2019-04-26 11:22:09 +0100

    TLS MQTT Trigger - Inital for testing

---
+++ trigger/mqtt/trigger.go
@@ -0,0 +1,268 @@
+package mqtt
+
+import (
+	"context"
+
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+	"time"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/data"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/eclipse/paho.mqtt.golang"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("trigger-ayh20-mqtt-tls")
+
+// MqttTrigger is simple MQTT trigger
+type MqttTrigger struct {
+	metadata       *trigger.Metadata
+	client         mqtt.Client
+	config         *trigger.Config
+	handlers       []*trigger.Handler
+	topicToHandler map[string]*trigger.Handler
+}
+
+//NewFactory create a new Trigger factory
+func NewFactory(md *trigger.Metadata) trigger.Factory {
+	return &MQTTFactory{metadata: md}
+}
+
+// MQTTFactory MQTT Trigger factory
+type MQTTFactory struct {
+	metadata *trigger.Metadata
+}
+
+//New Creates a new trigger instance for a given id
+func (t *MQTTFactory) New(config *trigger.Config) trigger.Trigger {
+	return &MqttTrigger{metadata: t.metadata, config: config}
+}
+
+// Metadata implements trigger.Trigger.Metadata
+func (t *MqttTrigger) Metadata() *trigger.Metadata {
+	return t.metadata
+}
+
+// Initialize implements trigger.Initializable.Initialize
+func (t *MqttTrigger) Initialize(ctx trigger.InitContext) error {
+	t.handlers = ctx.GetHandlers()
+	return nil
+}
+
+// Start implements trigger.Trigger.Start
+func (t *MqttTrigger) Start() error {
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(t.config.GetSetting("broker"))
+	opts.SetClientID(t.config.GetSetting("id"))
+	opts.SetUsername(t.config.GetSetting("user"))
+	opts.SetPassword(t.config.GetSetting("password"))
+	b, err := data.CoerceToBoolean(t.config.Settings["cleansess"])
+	if err != nil {
+		log.Error("Error converting \"cleansess\" to a boolean ", err.Error())
+		return err
+	}
+	opts.SetCleanSession(b)
+	if storeType := t.config.Settings["store"]; storeType != ":memory:" {
+		opts.SetStore(mqtt.NewFileStore(t.config.GetSetting("store")))
+	}
+	// Get settings for TLS (store location, thing name)
+	tlsEnabled, err := data.CoerceToBoolean(t.config.Settings["enabletls"])
+	if err != nil {
+		log.Error("Error converting \"enabletls\" to a boolean ", err.Error())
+		return err
+	}
+
+	ivCertStore := t.config.GetSetting("certstore")
+
+	ivThing := t.config.GetSetting("thing")
+
+	opts.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
+		topic := msg.Topic()
+		//TODO we should handle other types, since mqtt message format are data-agnostic
+		payload := string(msg.Payload())
+		log.Debug("Received msg:", payload)
+		handler, found := t.topicToHandler[topic]
+		if found {
+			t.RunHandler(handler, payload)
+		} else {
+			log.Errorf("handler for topic '%s' not found", topic)
+		}
+	})
+
+	//set tls config
+	if tlsEnabled {
+		// if thing is not set, this indicates that this is not AWS IoT
+		if ivThing == "" {
+			tlsConfig := NewTLSConfig(ivCertStore)
+			opts.SetTLSConfig(tlsConfig)
+		} else {
+			tlsConfig := AWSTLSConfig(ivThing, ivCertStore)
+			opts.SetTLSConfig(tlsConfig)
+		}
+	}
+
+	client := mqtt.NewClient(opts)
+	t.client = client
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	i, err := data.CoerceToDouble(t.config.Settings["qos"])
+	if err != nil {
+		log.Error("Error converting \"qos\" to an integer ", err.Error())
+		return err
+	}
+
+	t.topicToHandler = make(map[string]*trigger.Handler)
+
+	for _, handler := range t.handlers {
+
+		topic := handler.GetStringSetting("topic")
+
+		if token := t.client.Subscribe(topic, byte(i), nil); token.Wait() && token.Error() != nil {
+			log.Errorf("Error subscribing to topic %s: %s", topic, token.Error())
+			return token.Error()
+		}
+		log.Debugf("Subscribed to topic: %s, will trigger handler: %s", topic, handler)
+		t.topicToHandler[topic] = handler
+
+	}
+
+	return nil
+}
+
+// Stop implements ext.Trigger.Stop
+func (t *MqttTrigger) Stop() error {
+	//unsubscribe from topic
+	for _, handlerCfg := range t.config.Handlers {
+		log.Debug("Unsubscribing from topic: ", handlerCfg.GetSetting("topic"))
+		if token := t.client.Unsubscribe(handlerCfg.GetSetting("topic")); token.Wait() && token.Error() != nil {
+			log.Errorf("Error unsubscribing from topic %s: %s", handlerCfg.Settings["topic"], token.Error())
+		}
+	}
+
+	t.client.Disconnect(250)
+
+	return nil
+}
+
+// RunHandler runs the handler and associated action
+func (t *MqttTrigger) RunHandler(handler *trigger.Handler, payload string) {
+
+	trgData := make(map[string]interface{})
+	trgData["message"] = payload
+
+	results, err := handler.Handle(context.Background(), trgData)
+
+	if err != nil {
+		log.Error("Error starting action: ", err.Error())
+	}
+
+	log.Debugf("Ran Handler: [%s]", handler)
+
+	var replyData interface{}
+
+	if len(results) != 0 {
+		dataAttr, ok := results["data"]
+		if ok {
+			replyData = dataAttr.Value()
+		}
+	}
+
+	if replyData != nil {
+		dataJSON, err := json.Marshal(replyData)
+		if err != nil {
+			log.Error(err)
+		} else {
+			replyTo := handler.GetStringSetting("topic")
+			if replyTo != "" {
+				t.publishMessage(replyTo, string(dataJSON))
+			}
+		}
+	}
+}
+
+func (t *MqttTrigger) publishMessage(topic string, message string) {
+
+	log.Debug("ReplyTo topic: ", topic)
+	log.Debug("Publishing message: ", message)
+
+	qos, err := strconv.Atoi(t.config.GetSetting("qos"))
+	if err != nil {
+		log.Error("Error converting \"qos\" to an integer ", err.Error())
+		return
+	}
+	if len(topic) == 0 {
+		log.Warn("Invalid empty topic to publish to")
+		return
+	}
+	token := t.client.Publish(topic, byte(qos), false, message)
+
+	sent := token.WaitTimeout(5000 * time.Millisecond)
+	if !sent {
+		// Timeout occurred
+		log.Errorf("Timeout occurred while trying to publish to topic '%s'", topic)
+		return
+	}
+}
+
+// NewTLSConfig creates a TLS configuration using passed cert
+func NewTLSConfig(certstore string) *tls.Config {
+
+	// Import root CA
+	certpool := x509.NewCertPool()
+	pemCerts, err := ioutil.ReadFile(certstore)
+	if err == nil {
+		certpool.AppendCertsFromPEM(pemCerts)
+	}
+
+	return &tls.Config{
+		RootCAs:            certpool,
+		ClientAuth:         tls.NoClientCert,
+		ClientCAs:          nil,
+		InsecureSkipVerify: true,
+	}
+}
+
+// AWSTLSConfig creates a TLS configuration for the specified 'thing'
+func AWSTLSConfig(thingName string, thingDir string) *tls.Config {
+
+	thingLoc := thingDir
+
+	if thingLoc != "" {
+		thingLoc += "/"
+	}
+
+	// Import root CA
+	certpool := x509.NewCertPool()
+	pemCerts, err := ioutil.ReadFile(thingLoc + "root-CA.crt")
+	if err == nil {
+		certpool.AppendCertsFromPEM(pemCerts)
+	}
+
+	// Import client certificate/key pair for the specified 'thing'
+	cert, err := tls.LoadX509KeyPair(thingLoc+thingName+".cert.pem", thingLoc+thingName+".private.key")
+	if err != nil {
+		panic(err)
+	}
+
+	cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		panic(err)
+	}
+
+	return &tls.Config{
+		RootCAs:            certpool,
+		ClientAuth:         tls.NoClientCert,
+		ClientCAs:          nil,
+		InsecureSkipVerify: true,
+		Certificates:       []tls.Certificate{cert},
+	}
+}
---
+++ trigger/mqtt/trigger_test.go
@@ -0,0 +1,136 @@
+package mqtt
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	//MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+)
+
+var jsonTestMetadata = getTestJsonMetadata()
+
+func getTestJsonMetadata() string {
+	jsonMetadataBytes, err := ioutil.ReadFile("trigger.json")
+	if err != nil {
+		panic("No Json Metadata found for trigger.json path")
+	}
+	return string(jsonMetadataBytes)
+}
+
+const testConfig string = `{
+  "name": "flogo-mqtt",
+  "settings": {
+    "topic": "flogo/#",
+    "broker": "tcp://127.0.0.1:1883",
+    "id": "flogoEngine",
+    "user": "",
+    "password": "",
+    "store": "",
+    "qos": "0",
+    "cleansess": "false"
+  },
+  "handlers": [
+    {
+      "actionId": "device_info",
+      "settings": {
+        "topic": "test_start"
+      }
+    }
+  ]
+}`
+
+const testConfig2 string = `{
+	"name": "flogo-mqtt",
+	"settings": {
+	  "topic": "flogo/#",
+	  "broker": "ssl://mqtt.bosch-iot-hub.com:8883",
+	  "id": "flogoEngine",
+	  "user": "little-sensor@tcef56e88b16548f9a4a49cd5b92150af",
+	  "password": "plaintextPassword",
+	  "store": "",
+	  "qos": "1",
+	  "cleansess": "false",
+	  "enabletls": "true",
+	  "certstore": "C:/Users/ahampshi/Documents/BoschIoTStuff/iothub.crt"
+
+	},
+	"handlers": [
+	  {
+		"actionId": "device_info",
+		"settings": {
+		  "topic": "event"
+		}
+	  }
+	]
+  }`
+
+func TestInit(t *testing.T) {
+
+	// New  factory
+	md := trigger.NewMetadata(jsonTestMetadata)
+	f := NewFactory(md)
+
+	// New Trigger
+	config := trigger.Config{}
+	json.Unmarshal([]byte(testConfig), config)
+	f.New(&config)
+
+	json.Unmarshal([]byte(testConfig2), config)
+	f.New(&config)
+
+}
+
+/*
+// TODO Fix this test
+func TestEndpoint(t *testing.T) {
+
+	// New  factory
+	md := trigger.NewMetadata(jsonMetadata)
+	f := NewFactory(md)
+
+	// New Trigger
+	config := trigger.Config{}
+	json.Unmarshal([]byte(testConfig), &config)
+	tgr := f.New(&config)
+
+	runner := &TestRunner{}
+
+	tgr.Init(runner)
+
+	tgr.Start()
+	defer tgr.Stop()
+
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("tcp://127.0.0.1:1883")
+	opts.SetClientID("flogo_test")
+	opts.SetUsername("")
+	opts.SetPassword("")
+	opts.SetCleanSession(false)
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	log.Debug("---- doing first publish ----")
+
+	token := client.Publish("test_start", 0, false, "Test message payload!")
+	token.Wait()
+
+	duration2 := time.Duration(2)*time.Second
+	time.Sleep(duration2)
+
+	log.Debug("---- doing second publish ----")
+
+	token = client.Publish("test_start", 0, false, "Test message payload!")
+	token.Wait()
+
+	duration5 := time.Duration(5)*time.Second
+	time.Sleep(duration5)
+
+	client.Disconnect(250)
+	log.Debug("Sample Publisher Disconnected")
+}
+*/
