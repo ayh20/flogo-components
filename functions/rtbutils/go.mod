4d08adfb62c7ce82be39a65f95cd16e19d5173f0
Andy Hampshire <ahampshi@tibco.com>
2021-05-27 12:06:22 +0100

    Fix version issues

--- functions/rtbutils/go.mod
+++ functions/rtbutils/go.mod
@@ -1,10 +1,8 @@
 module github.com/ayh20/flogo-components/functions/rtbutils
 
-go 1.12
+go 1.16
 
 require (
-	github.com/TIBCOSoftware/flogo-lib v0.5.8
-	github.com/project-flogo/core v0.9.0
-	github.com/sirupsen/logrus v1.4.1 // indirect
-	github.com/stretchr/testify v1.3.0
+	github.com/project-flogo/core v1.4.0
+	github.com/stretchr/testify v1.4.0
 )
--- functions/rtbutils/timestamp_test.go
+++ functions/rtbutils/timestamp_test.go
@@ -3,7 +3,6 @@ package rtbutils
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/project-flogo/core/data/expression/function"
 	"github.com/stretchr/testify/assert"
@@ -17,7 +16,7 @@ func TestFntimestamp_Eval(t *testing.T) {
 	assert.Nil(t, err)
 	//assert.Equal(t, Time.Format("02-Jan-2006"), v)
 	fmt.Println(v)
-	v, err = function.Eval(f, time.Now(), "2006-01-02 15:04:05.999")
+	v, err = function.Eval(f, "2006-01-02 15:04:05.999")
 
 	assert.Nil(t, err)
 	fmt.Println(v)
