21d34fb77b9042dcd6bac3683f6ca77b5cc17ec7
Andy Hampshire <andy@starbeast.org>
2023-02-28 09:00:18 +0000

    Update activity_test.go

--- activity/parsecsv/activity_test.go
+++ activity/parsecsv/activity_test.go
@@ -68,6 +68,39 @@ func TestEval(t *testing.T) {
 	fmt.Println(string(b))
 }
 
+func TestUTFEval(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	fields := make([]interface{}, 3)
+	fields[0] = "test"
+	fields[1] = "test2"
+	fields[2] = "test3"
+	tc.SetInput("fieldNames", fields)
+	tc.SetInput("delimiter", ",")
+	tc.SetInput("csv", "hello,Quels sont les avantages de créer davantage de « container instances » dans TIBCO BPM Enterprise ?,name\ntest,2H₂ + O₂ ⇌ 2H₂O R = 4.7 kΩ ⌀ 200 mm,test")
+
+	done, err := act.Eval(tc)
+	if !done {
+		fmt.Println(err)
+	}
+
+	//check result attr
+	b, _ := json.Marshal(tc.GetOutput("output"))
+	fmt.Println("UTF8")
+	fmt.Printf("Array: %v", b)
+	fmt.Println(string(b))
+}
+
 func TestFile(t *testing.T) {
 	defer func() {
 		if r := recover(); r != nil {
