587acefb9ced17a5f394d392245d93773e679d87
Andy Hampshire <andy@starbeast.org>
2023-05-02 08:34:29 +0100

    Remove stored password

--- activity/gremlin/activity_test.go
+++ activity/gremlin/activity_test.go
@@ -2,6 +2,7 @@ package gremlin
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/project-flogo/core/activity"
@@ -19,7 +20,8 @@ func TestRegister(t *testing.T) {
 
 func TestPlain(t *testing.T) {
 
-	settings := &Settings{GremlinUrls: "wss://qbe-cosmosdb-gml.gremlin.cosmos.azure.com:443", User: "/dbs/vehicle_events_db/colls/vehicle_events_graph", Password: "AjEPsihz85GUAEwMTJYc4CqXOKn5c1PH6oe7Ggk55mzqSmSWBYYAgSqwWkaDJezQAN3DSXrst2zZACDbvJ9ZHQ=="}
+	passw := os.Getenv("GREMLIN_PWD")
+	settings := &Settings{GremlinUrls: "wss://qbe-cosmosdb-gml.gremlin.cosmos.azure.com:443", User: "/dbs/vehicle_events_db/colls/vehicle_events_graph", Password: passw}
 
 	iCtx := test.NewActivityInitContext(settings, nil)
 	act, err := New(iCtx)
